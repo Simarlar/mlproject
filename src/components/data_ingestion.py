40886dfb44f12c2ceb45690c8b8e3011d72402f4
SimarLar <simakumariktg147@gmail.com>
2026-03-18 20:08:33 +0630

    Add Model Trainer Code

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -6,6 +6,7 @@ from src.components.data_transformation import DataTransformationConfig
 from src.logger import logging
 from src.exception import CustomException
 from sklearn.model_selection import train_test_split
+from src.components.model_trainer import ModelTrainerConfig,ModelTrainer
 from dataclasses import dataclass
 
 
@@ -44,6 +45,9 @@ if __name__ == "__main__":
     obj = DataIngestion()
     train_data,test_data,raw_data = obj.initiate_data_ingestion()
     data_transformation = DataTransformation()
-    data_transformation.initiate_data_transformation(train_data,test_data)
+    train_arr,test_arr,_= data_transformation.initiate_data_transformation(train_data,test_data)
+    modeltrainer = ModelTrainer()
+    print(modeltrainer.initiate_model_trainer(train_arr,test_arr))
+    
     
 
